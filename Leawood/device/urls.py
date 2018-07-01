cb5aadafaec184c3fb06ae77d9c15ef9ddd6eb5f
Steve Mayze <smayze@yahoo.com>
2018-07-01 22:08:29 +0200

    Merged the device list and the register pages in one

--- Leawood/device/urls.py
+++ Leawood/device/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls import url
 from .views import (
 	device_list,
 	device_create,
-	device_scan,
 	device_detail,
 	device_update,
 	device_delete
@@ -16,7 +15,6 @@ app_name = 'device'
 urlpatterns = [
     url(r'^$', device_list, name='list'),
     url(r'^create/$', device_create, name='create'),
-    url(r'^scan/$', device_scan, name='scan'),
     url(r'^(?P<id>\d+)$', device_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit$', device_update, name='update'),
     url(r'^(?P<id>\d+)/delete/$', device_delete, name='device_delete'),
--- Leawood/device/views.py
+++ Leawood/device/views.py
@@ -17,33 +17,80 @@ def check_user( request ):
 
     
 def device_list( request ):
-	queryset_list = Field_Device.objects.filter(registered = True )
 	
 	query = request.GET.get("q")
-	if query:
+	current_tab = request.GET.get("t")
+	if current_tab == None:
+		current_tab = "list"
+		
+	if request.method == 'POST':
+		current_tab = request.POST.get("t")
+		payload = request.POST
+		print(">> POST: {0}".format(request.POST))
+		print(">> content_params: {0}".format(request.content_params))
+		for item in payload:
+			if item.startswith('device: '):
+				print(">> ITEM: {0} = {1}".format(item, payload[item]))
+				tmp, device_id = item.split(":")
+				device = Field_Device.objects.get(id = device_id)
+				device.registered = True
+				device.save()
+			else:
+				print("UNMATCHED ITEM {0}".format(item))
+		
+	page_request_var = "page"
+	tab_request_var = "t"		
+	
+	queryset_list = Field_Device.objects.filter(registered = True )
+	if query and current_tab == "list":
 		queryset_list = queryset_list.filter(
 				Q(name__icontains=query) | 
 				Q(description__icontains=query)
 			).distinct()
-	page_request_var = "page"
-	paginator = Paginator(queryset_list, 5)
+
+
+	queryset_register = Field_Device.objects.filter(registered = False )
+	if query and current_tab == "register":
+		queryset_register = queryset_register.filter(
+				Q(name__icontains=query) | 
+				Q(description__icontains=query)
+			).distinct()
+			
+			
+	paginator_list = Paginator(queryset_list, 5)
+	paginator_register = Paginator(queryset_register, 5)
 	page = request.GET.get(page_request_var)
-	try:	
-		queryset = paginator.page(page)
+	try:
+		print("PAGINATOR LIST PAGE={0}".format(page))
+		queryset_l = paginator_list.page(page)
 	except PageNotAnInteger:
-		queryset = paginator.page(1)
+		queryset_l = paginator_list.page(1)
 	except EmptyPage:
-		queryset = paginator.page(paginator.num_pages)
+		print("PAGINATOR LIST EXCEPTION - EMPTY PAGE")
+		queryset_l = paginator_list.page(paginator_list.num_pages)
+	try:
+		print("PAGINATOR REGISTER PAGE={0}".format(page))
+		queryset_r = paginator_register.page(page)
+	except PageNotAnInteger:
+		queryset_r = paginator_register.page(1)
+	except EmptyPage:
+		print("PAGINATOR REGISTER EXCEPTION - EMPTY PAGE")
+		queryset_r = paginator_register.page(paginator_register.num_pages)
+
+
 
 	context_dict = {}
 	context_dict["pagetitle"] = "Leawood"
 	context_dict["pagename"] = "Devices"
 	context_dict["titlebar"] = "Leawood - Devices"
-	context_dict["object_list"] = queryset
+	context_dict["object_list"] = queryset_l
+	context_dict["object_register"] = queryset_r
 	context_dict["page_request_var"] = page_request_var
+	context_dict["tab_request_var"] = tab_request_var
+	context_dict["current_tab"] = current_tab
 	
 	
-	response = render(request, 'device/list.htm', context=context_dict)
+	response = render(request, 'device/device.htm', context=context_dict)
 	return response
 
 
@@ -81,51 +128,6 @@ def device_create( request ):
 	response = render(request, 'device/device_form.htm', context=context_dict)
 	return response
 	
-def device_scan( request ):
-	
-	if request.method == 'POST':
-		payload = request.POST
-		print(">> POST: {0}".format(request.POST))
-		print(">> content_params: {0}".format(request.content_params))
-		for item in payload:
-			if item.startswith('device: '):
-				print(">> ITEM: {0} = {1}".format(item, payload[item]))
-				tmp, device_id = item.split(":")
-				device = Field_Device.objects.get(id = device_id)
-				device.registered = True
-				device.save()
-			else:
-				print("UNMATCHED ITEM {0}".format(item))
-				
-	
-	queryset_list = Field_Device.objects.filter(registered = False)
-	
-	query = request.GET.get("q")
-	if query:
-		queryset_list = queryset_list.filter(
-				Q(name__icontains=query) | 
-				Q(description__icontains=query)
-			).distinct()
-	page_request_var = "page"
-	paginator = Paginator(queryset_list, 5)
-	page = request.GET.get(page_request_var)
-	try:	
-		queryset = paginator.page(page)
-	except PageNotAnInteger:
-		queryset = paginator.page(1)
-	except EmptyPage:
-		queryset = paginator.page(paginator.num_pages)
-	
-	context_dict = {}
-	context_dict["pagetitle"] = "Leawood"
-	context_dict["pagename"] = "Device Scan"
-	context_dict["titlebar"] = "Leawood - Device Scan"
-	context_dict["object_list"] = queryset
-	context_dict["page_request_var"] = page_request_var
-
-	response = render( request, 'device/device_scan.htm', context=context_dict )
-	return response	
-		
 	
 def device_update( request, id=None ):
 	
